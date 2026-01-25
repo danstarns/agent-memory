55a86c7c7228a7f7c10ce1813f17501cc60dd7d5
William Lyon <lyonwj@gmail.com>
2026-01-25 04:49:06 -0700

    update styles

--- docs/build.js
+++ docs/build.js
@@ -423,6 +423,10 @@ function wrapInTemplate(content, sourceFile, docTitle, navigation, rootPath) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${title} | Neo4j Agent Memory</title>
   <link rel="icon" type="image/svg+xml" href="${rootPath}favicon.svg">
+  <!-- Google Fonts: Neo4j Brand Typography -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&family=Syne:wght@600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="${rootPath}style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <link id="hljs-theme" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css">
